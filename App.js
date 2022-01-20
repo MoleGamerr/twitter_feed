8cbe8d26006bb6235f5efd8c482e31510bad6b0d
gints_perkons <gintskristaps13@gmail.com>
2022-01-20 21:37:11 +0200

    main parts

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { FeedProvider } from './components/Feed';
+import Tweet from './components/Tweet';
+
+
+function HomeScreen() {
+
+  const apiKey = "76972041e9fe4dbfbbce6048a9bc20c9";
+  const apiSecret = "8yWCtWmVsxjZOvNIuvCEHtop7kGRhIE1NWvT8IlLC6LGrWXpJy";
+  const site ="https://newsapi.org/v2/top-headlines";
+
+  
+
+  return (
+    
+    <FeedProvider><Tweet /></FeedProvider>
+  )
+
+}
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator >
+        <Stack.Screen name="Tweets" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
