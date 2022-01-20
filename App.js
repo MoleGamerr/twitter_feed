0ff3f1032fe16372e2444db9c983a145bba6a810
gints_perkons <gintskristaps13@gmail.com>
2022-01-20 21:42:19 +0200

    main parts

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { FeedProvider } from './components/Feed';
@@ -7,10 +5,6 @@ import Tweet from './components/Tweet';
 
 
 function HomeScreen() {
-
-  const apiKey = "76972041e9fe4dbfbbce6048a9bc20c9";
-  const apiSecret = "8yWCtWmVsxjZOvNIuvCEHtop7kGRhIE1NWvT8IlLC6LGrWXpJy";
-  const site ="https://newsapi.org/v2/top-headlines";
 
   
 
